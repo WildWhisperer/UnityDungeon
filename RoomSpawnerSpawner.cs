976799c34f61bde88135bbb14c40aca7c30eef7a
WildWhisperer <Ramish.thegamer@gmail.com>
2016-03-20 18:05:42 +0500

    DungeonGeneration
    Version incomplete wallcollider only horizontal

---
+++ RoomSpawnerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RoomSpawnerSpawner : MonoBehaviour {
+
+
+    public GameObject roomSpawner;
+    public int radius;
+    public static int maxRooms = 50;
+    public int maxRoomz;
+    void Start ()
+    {
+        maxRooms = maxRoomz;
+        for (int x = 0; x < maxRooms; x++)
+        {
+            Vector3 Pos = new Vector3(Random.Range(-radius, radius), Random.Range(-radius, radius), 0);
+            GameObject roomSpawnerTemp = Instantiate(roomSpawner, Pos, Quaternion.identity) as GameObject;
+            roomSpawnerTemp.name = "" + x;
+        }
+    }
+
+
+}
---
+++ WallColliderGenerator.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WallColliderGenerator : MonoBehaviour {
+    public static List<Vector3> WalledWalls = new List<Vector3>();
+    public bool hasWalled = false;
+
+    public void ColliderHorizontal()
+    {
+        BoxCollider2D CurrentCollider = null;
+
+        if (WalledWalls.Contains(transform.position) || hasWalled == true)
+        {
+            return;
+        }
+        if (!WalledWalls.Contains(transform.position))
+        {
+            WalledWalls.Add(transform.position);
+            CurrentCollider = gameObject.AddComponent<BoxCollider2D>();
+        }
+
+        Vector3 rightWallPos = transform.position + Vector3.right;
+        while (RoomSpawner.wallPosition.Contains(rightWallPos))
+        {
+            if (!WalledWalls.Contains(rightWallPos))
+            {
+                WalledWalls.Add(rightWallPos);
+                CurrentCollider.size += new Vector2(1, 0);
+                CurrentCollider.offset += new Vector2(0.5f, 0);
+                rightWallPos += Vector3.right;
+            }
+           
+        }
+
+
+        Vector3 leftWallPos = transform.position + Vector3.left;
+        while (RoomSpawner.wallPosition.Contains(leftWallPos))
+        {
+            if (!WalledWalls.Contains(leftWallPos))
+            {
+                WalledWalls.Add(leftWallPos);
+                CurrentCollider.size += new Vector2(1, 0);
+                CurrentCollider.offset += new Vector2(-0.5f, 0);
+                leftWallPos += Vector3.left;
+            }
+                  
+        }
+      //  ColliderVertical();
+    }
+
+    public void ColliderVertical()
+    {
+        BoxCollider2D CurrentCollider = null;
+        if (WalledWalls.Contains(transform.position) || hasWalled == true)
+        {
+            return;
+        }
+        if (!WalledWalls.Contains(transform.position))
+        {
+            WalledWalls.Add(transform.position);
+            CurrentCollider = gameObject.AddComponent<BoxCollider2D>();
+        }
+
+        Vector3 UpWallPos = transform.position + Vector3.up;
+        while (RoomSpawner.wallPosition.Contains(UpWallPos))
+        {
+            if (!WalledWalls.Contains(UpWallPos))
+            {
+                WalledWalls.Add(UpWallPos);
+                CurrentCollider.size += new Vector2(0, 1);
+                CurrentCollider.offset += new Vector2(0, 0.5f);
+                UpWallPos += Vector3.up;
+            }
+        }
+
+        Vector3 downWallPos = transform.position + Vector3.down;
+        while (RoomSpawner.wallPosition.Contains(downWallPos))
+        {
+            if (!WalledWalls.Contains(downWallPos))
+            {
+                WalledWalls.Add(downWallPos);
+                CurrentCollider.size += new Vector2(0, 1);
+                CurrentCollider.offset += new Vector2(0, -0.5f);
+                downWallPos += Vector3.down;
+            }          
+        }   
+    }
+
+    void Update()
+    {
+        if (WalledWalls.Contains(transform.position))
+        {
+            hasWalled = true;
+        }
+    }
+
+
+
+}
